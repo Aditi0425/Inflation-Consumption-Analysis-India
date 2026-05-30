e33f67e2219d1ead54ec9db1fe52f3c6e7406287
Aditi Garg <aditigarg0925@gmail.com>
2026-05-30 15:43:08 +0530

    Update IC project R file.R

--- IC project R file.R
+++ IC project R file.R
@@ -1,11 +1,4 @@
-directory <- "C:/Users/User/Desktop/Project/"
-setwd("C:/Users/User/Desktop/Project/")
-
 IC_data<- read.csv("ic data.csv")
-
-str(IC_data)
-summary(IC_data)
-View(IC_data)
 
 # correlation matrix
 vars <- IC_data[, c("PFCE_growth",
@@ -17,18 +10,13 @@ round(cor(vars),2)
 corrplot(cor(vars), method="color")
 
 # linear regression
-
 model <- lm(PFCE_growth ~ food_inflation +
                fuel_inflation +
                PCI_growth +
                covid_dummy,IC_data)
 summary(model)
-# coefficient of covid dummy is now significant at 15% level 
+ 
 durbinWatsonTest(model)
-
 vif(model)
-
 bptest(model)
 plot(model)
-
-coeftest(model,vcov=NeweyWest(model))
